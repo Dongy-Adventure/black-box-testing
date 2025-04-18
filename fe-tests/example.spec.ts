878552fea114329c3e3a5100f36bb37ea9d9ee38
D33102 <dalai2547@gmail.com>
2025-04-18 16:16:48 +0700

    test automation

--- fe-tests/example.spec.ts
+++ fe-tests/example.spec.ts
@@ -1,18 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const BASE_URL = process.env.URL || "http://localhost:3000";
+
+test("Has Title", async ({ page }) => {
+  await page.goto(BASE_URL);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Tar's Marketplace/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Has registration page", async ({ page }) => {
+  await page.goto(BASE_URL);
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Register" })).toBeVisible();
 });
